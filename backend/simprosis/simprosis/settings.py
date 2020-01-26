1f157211ff7b2f891bd8d8a13a7eb28c92578500
ristykdr <ristykdr@gmail.com>
2020-01-26 21:45:36 +0700

    Changes to be committed: 	new file:   backend/envsi/Scripts/wheel.exe 	new file:   backend/envsi/mysqlclient-1.4.2-cp37-cp37m-win_amd64.whl 	modified:   backend/simprosis/simprosis/settings.py 	modified:   backend/simprosis/simprosis/urls.py 	new file:   backend/simprosis/simprosis/views.py

--- backend/simprosis/simprosis/settings.py
+++ backend/simprosis/simprosis/settings.py
@@ -74,9 +74,19 @@ WSGI_APPLICATION = 'simprosis.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # Using sqlite3
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'    : 'django.db.backends.mysql',
+        'NAME'      : 'simprosis',
+        'USER'      : 'root',
+        'PASSWORD'  : '',
+        'HOST'      : 'localhost',
+        'PORT'      : '3306'
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
